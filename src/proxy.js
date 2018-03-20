0b71409a91ddd46e1a089b89cc032fb848d2e653
Ben Evans <ben@bluechimp.io>
2018-03-20 01:20:33 +0000

    logging

--- src/proxy.js
+++ src/proxy.js
@@ -1,8 +1,12 @@
 const http = require('http')
 const https = require('https')
+const morgan = require('morgan')
 
 const createProxy = (b2, bucketName) => {
+  const log = morgan('combined');
+
   return http.createServer((servReq, servRes) => {
+    log(servReq, servRes, () => {})
     const target = b2.downloadUrl + '/file/' + bucketName + servReq.url
 
     https.get(target, (b2Res) => {
